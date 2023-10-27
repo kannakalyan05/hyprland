699a5ce33aea5f0a40a75af1f71570029a0ddde7
Kanna kalyan <kannakalyan05@gmail.com>
2023-10-27 21:41:15 +0530

    done with nvidia

--- nvidia.sh
+++ nvidia.sh
@@ -9,48 +9,22 @@ nvidia_stage=(
     libva-nvidia-driver-git
 )
 
-for str in ${myArray[@]}; do
-  echo $str
-done
-
-# set some colors
-CNT="[\e[1;36mNOTE\e[0m]"
-COK="[\e[1;32mOK\e[0m]"
-CER="[\e[1;31mERROR\e[0m]"
-CAT="[\e[1;37mATTENTION\e[0m]"
-CWR="[\e[1;35mWARNING\e[0m]"
-CAC="[\e[1;33mACTION\e[0m]"
-INSTALOG="install.log"
-######
-
-# functions go here
-# function that would show a progress bar to the user
-show_progress() {
-    while ps | grep $1 &> /dev/null;
-    do
-        echo -n "."
-        sleep 2
-    done
-    echo -en "Done!\n"
-    sleep 2
-}
 
 # function that will test for a package and if not found it will attempt to install it
 install_software() {
     # First lets see if the package is there
     if yay -Q $1 &>> /dev/null ; then
-        echo -e "$COK - $1 is already installed."
+        echo -e "$1 is already installed."
     else
         # no package found so installing
-        echo -en "$CNT - Now installing $1 ."
-        yay -S --noconfirm $1 &>> $INSTLOG &
-        show_progress $!
+        echo -en "Now installing $1 ."
+        yay -S $1
         # test to make sure package installed
         if yay -Q $1 &>> /dev/null ; then
-            echo -e "\e[1A\e[K$COK - $1 was installed."
+            echo -e "$1 was installed."
         else
             # if this is hit then a package is missing, exit to review log
-            echo -e "\e[1A\e[K$CER - $1 install had failed, please check the install.log"
+            echo -e "$1 install had failed."
             exit
         fi
     fi
@@ -58,16 +32,6 @@ install_software() {
 
 # clear the screen
 clear
-
-# give the user an option to exit out
-read -rep $'[\e[1;33mACTION\e[0m] - Would you like to continue with the install (y,n) ' CONTINST
-if [[ $CONTINST == "Y" || $CONTINST == "y" ]]; then
-    echo -e "$CNT - Setup starting..."
-    sudo touch /tmp/hyprv.tmp
-else
-    echo -e "$CNT - This script will now exit, no changes were made to your system."
-    exit
-fi
 
 # find the Nvidia GPU
 if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
@@ -83,7 +47,7 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
 
     # Setup Nvidia if it was found
     if [[ "$ISNVIDIA" == true ]]; then
-        echo -e "$CNT - Nvidia GPU support setup stage, this may take a while..."
+        echo -e "Nvidia GPU support setup stage, this may take a while..."
         for SOFTWR in ${nvidia_stage[@]}; do
             install_software $SOFTWR
         done
@@ -91,8 +55,6 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
         # update config
         sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
         sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-        echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+        echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
     fi
-
 fi
-
