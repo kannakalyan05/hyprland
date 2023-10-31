01f1f5967dfabfc749f3f2607ff1ae52087ff991
Kanna kalyan <kannakalyan05@gmail.com>
2023-10-31 10:18:31 +0530

    Last updated on 31/10/2023 10:18:31

--- config/hypr/autostart.sh
+++ config/hypr/autostart.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if [ -f "/usr/bin/swayidle" ]; then
-  echo "swayidle is installed."
-  swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
-else
-  echo "swayidle is not installed."
-fi;
-
 # Other commands from your first script (e.g., setting the background and restarting Waybar)
 set_current_background() {
   swaymsg output '*' bg-image ~/.config/backgrounds/forest.jpg
