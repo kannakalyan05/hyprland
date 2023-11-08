7684a0931d498cf3fa637999af483ca0caab17c2
Kanna kalyan <kannakalyan05@gmail.com>
2023-11-08 16:12:25 +0530

    Last updated on 08/11/2023 16:12:25

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ if [[ $CFG == "Y" || $CFG == "y" ]]; then
 
 # Setup each appliaction
     # check for existing config folders and backup 
-    for DIR in backgrounds hypr kitty mako swaylock nwg-look waybar qt5ct qt6ct rofi wlogout neofetch gtk-3.0
+    for DIR in backgrounds hypr kitty wofi mako swaylock nwg-look waybar qt5ct qt6ct rofi wlogout neofetch gtk-3.0
     do 
         DIRPATH=~/.config/$DIR
         if [ -d "$DIRPATH" ]; then 
