8d3a196fec10dc7eb7f858d8d8d40ace669cacbe
Kanna kalyan <kannakalyan05@gmail.com>
2023-11-09 18:18:41 +0530

    Last updated on 09/11/2023 18:18:41

--- config/hypr/autostart.sh
+++ config/hypr/autostart.sh
@@ -11,7 +11,7 @@ restart_waybar() {
 }
 
 sway_idle() {
-   swayidle -w timeout 300 'swaylock -f' timeout 310 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+   swayidle -w timeout 10 'swaylock -f' timeout 15 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
 
 }
 
