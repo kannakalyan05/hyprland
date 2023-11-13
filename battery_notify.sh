ebc69e96acca9d6c306242b2a0f3a89da2e3b918
Kanna kalyan <kannakalyan05@gmail.com>
2023-11-13 20:02:18 +0530

    Last updated on 13/11/2023 20:02:18

---
+++ battery_notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the battery percentage without the % symbol
+percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | awk '/percentage/ {gsub(/%/, ""); print $2}')
+
+# Check if the percentage is below 30
+if [ "$percentage" -lt 30 ]; then
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u critical "Battery Level Critical" 
+else
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u normal "Battery Level Normal"
+fi
+
